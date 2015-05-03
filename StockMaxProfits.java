8ac7ed662f4684c1059cd956e8c7294b3ab6a35a
wumengzhou <wumengzhou@gmail.com>
2015-05-03 19:00:24 -0400

    Stock Max - Greedy

--- StockMaxProfits.java
+++ StockMaxProfits.java
@@ -9,5 +9,38 @@ public class StockMaxProfits {
 		// TODO Auto-generated method stub
 
 	}
+	
+	public static long calProfit(int[] prices) {
+        if (prices == null || prices.length < 2) {
+            return 0;
+        }
+        int len = prices.length;
+        Stack<Integer> highes = new Stack<Integer>();
+        int localHigh = prices[len-1];
+        for (int i=len-2; i>=0; i--) {
+            if (prices[i] < prices[i+1] && prices[i+1] >= localHigh) {
+                highes.push(i+1);
+                localHigh = prices[i+1];
+            }
+        }
+        if (highes.isEmpty()) {
+            return 0;
+        }
+        int index = 0, high = highes.pop();
+        long profit = 0;
+        while (index < len) {
+            while (index < high) {
+                profit += prices[high] - prices[index];
+                index++;
+            }
+            index = high+1;
+            if (highes.isEmpty()) {
+                break;
+            } else {
+                high = highes.pop();
+            }
+        }
+        return profit;
+    }
 
 }
