9b407870e7362ed9cba25e4d5fb148e54456efd4
Shaun Jia <jiaxiaoyuan2008@gmail.com>
2015-05-03 21:42:51 -0400

    my recursive solution

--- StockMaxProfits.java
+++ StockMaxProfits.java
@@ -1,5 +1,7 @@
 package Zcode.Z_code;
 
+import java.util.Stack;
+
 public class StockMaxProfits {
 
 	/**
@@ -7,7 +9,9 @@ public class StockMaxProfits {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] arr = {3, 5, 2, 8, 7, 6, 1, 12, 22, 3, 41, 3, 7};
+		System.out.println(calProfit(arr));
+		System.out.println(calProfitSJ(arr));
 	}
 	
 	public static long calProfit(int[] prices) {
@@ -43,4 +47,29 @@ public class StockMaxProfits {
         return profit;
     }
 
+	public static long calProfitSJ(int[] prices) {
+		if(prices == null || prices.length < 2) {
+			return 0;
+		}
+		long max = 0;
+		return helper(prices, 0);
+	}
+	
+	private static long helper(int[] prices, int start) {
+		if(start >= prices.length) {
+			return 0;
+		}
+		int highestIndex = start;
+		for(int i=start+1; i<prices.length; i++) {
+			if(prices[i] > prices[highestIndex]) {
+				highestIndex = i;
+			}
+		}
+		long profit = 0;
+		int hightest = prices[highestIndex];
+		for(int j=start; j<highestIndex; j++) {
+			profit +=  hightest - prices[j];
+		}
+		return profit + helper(prices, highestIndex+1);
+	}
 }
