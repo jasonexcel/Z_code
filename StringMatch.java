aa2db970b3c975f3af29bcf6693badff209f8b21
Shaun Jia <jiaxiaoyuan2008@gmail.com>
2015-05-03 23:23:02 -0400

    bst

---
+++ StringMatch.java
@@ -0,0 +1,18 @@
+package Zcode.Z_code;
+
+public class StringMatch {
+
+	/**
+	 * @param args
+	 * String s1 = "waeginsapnaabangpisebbasepgnccccapisdnfngaabndlrjngeuiogbbegbuoecccc";
+String s2 = "a+b+c-";
+
+s2的形式是一个字母加上一个符号，正号代表有两个前面的字符，负号代表有四个，也就是说s2其实是"aabbcccc"，不考虑invalid。
+在s1中，找出连续或者不连续的s2，也就是说从s1中找出"aa....bb.....cccc"，abc顺序不能变，但是之间可以有零个或多个字符，返回共有多少个。在上面这个例子中，有四个。
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
